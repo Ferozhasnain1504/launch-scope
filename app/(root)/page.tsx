1c4a2fc6e3f4843e28c59fe4ebea71391e527bad
Feroz <ferozhasnain558@gmail.com>
2025-11-02 15:22:56 +0530

    Add startup card component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+import { Star } from "lucide-react";
+import { title } from "process";
 
 export default async function Home({searchParams}: { searchParams: Promise< {query?: string}> } ) {
 
   const query = (await searchParams).query;
+
+  const posts = [{_createdAt : new Date(),
+    views : 55,
+    author : { _id : 1, name : 'John Doe' },
+    _id : 1,
+    description : 'This is a sample description',
+    image : "https://img6.arthub.ai/64e9dd6d-377a.webp",
+    category : "Robots",
+    title : "AI Robot Assistant",
+   }];
+
   return (
     <>
      <section className="pink_container">
@@ -13,6 +27,22 @@ export default async function Home({searchParams}: { searchParams: Promise< {que
        </p>
 
        <SearchForm query = {query}/>
+     </section>
+
+     <section className="section_container"> 
+        <p className="text-30-semibold">
+          {query ? `Showing results for "${query}"` : 'Explore trending startup ideas below.'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          { posts?.length > 0 ? (
+            posts.map((post : StartupCardType, index: number) => (
+              <StartupCard key={post?._id}  post={post}/>
+            ))
+          ) : (
+            <p className="no-resutls">No startups found</p>
+          )}
+        </ul>
      </section>
     </>
   );
