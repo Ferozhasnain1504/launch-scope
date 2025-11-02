5b3496d3e3455fa63a02323284ac75bfbd326e85
Feroz <ferozhasnain558@gmail.com>
2025-11-02 10:36:10 +0530

    Change ui & configured shadcn lib

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-     <h1 className="text-2xl">Home</h1>
+     <section className="pink_container">
+       <h1 className="heading">Ignite Ideas, <br/> Build Together</h1>
+
+       <p className="sub-heading !max-w-3xl">
+          Pitch your idea, earn votes, and shine in virtual startup battles.
+       </p>
+     </section>
     </>
   );
 }
