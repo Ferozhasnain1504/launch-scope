e2f2920829fbfba7574cc23b9c7e3421ed0b85fa
Feroz <ferozhasnain558@gmail.com>
2025-11-16 20:20:05 +0530

    Views Addon use SSR and whole page use ISR which means PPR is implemented

--- components/View.tsx
+++ components/View.tsx
@@ -1,20 +1,25 @@
 import { START_VIEWS_QUERY } from "@/sanity/lib/queries"
 import Ping from "./Ping"
-import {client} from "@/sanity/lib/client"
+import { client } from "@/sanity/lib/client"
+import { ViewIncrementer } from "./ViewIncrementer"
 
-const View = async ({id} : {id:string}) => {
-  
-  const {views : totalViews} = await client.withConfig({useCdn : false}).fetch(START_VIEWS_QUERY, {id})
+const View = async ({ id }: { id: string }) => {
+  const { views: totalViews } = await client
+    .withConfig({ useCdn: false })
+    .fetch(START_VIEWS_QUERY, { id })
 
   return (
     <div className="view-container">
-        <div className="absolute -top-2 -right-2">
-            <Ping />
-        </div>
+      <div className="absolute -top-2 -right-2">
+        <Ping />
+      </div>
 
-        <p className="view-text">
-            <span className="font-black">Views: {totalViews}</span>
-        </p>
+      <p className="view-text">
+        <span className="font-black">Views: {totalViews}</span>
+      </p>
+
+      {/* triggers increment on the client */}
+      <ViewIncrementer id={id} views={totalViews} />
     </div>
   )
 }
